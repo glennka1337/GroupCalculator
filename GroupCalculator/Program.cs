99324022a6e81f2a19493954e92ab3932e6ed0a9
glennka1337 <robloxfan03@gmail.com>
2024-09-23 13:27:24 +0200

    Add project files.

---
+++ GroupCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GroupCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
