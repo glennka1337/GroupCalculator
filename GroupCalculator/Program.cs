9e03033a20475fbf87617ea76cfaaf4c6f2a11c9
glennka1337 <robloxfan03@gmail.com>
2024-09-23 13:52:58 +0200

    Fixade

--- GroupCalculator/Program.cs
+++ GroupCalculator/Program.cs
@@ -7,10 +7,13 @@
             Console.WriteLine("Hello, World!");
         }
 
-        static int Multiplication(int userInput1, int userInput2)
+        static void Multiplication()
         {
+            Console.WriteLine("Ange två heltal:");
+            int userInput1 = int.Parse(Console.ReadLine());
+            int userInput2 = int.Parse(Console.ReadLine());
             int sum = userInput1 * userInput2;
-            return sum;
+            Console.WriteLine($"Svaret är: {sum}");
         }
     }
 }
