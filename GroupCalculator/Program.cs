6bb05d7cf4d41c5309cd72ca1189f5d7c4921e9f
ArbunitShala <arbunitshalaa@gmail.com>
2024-09-23 14:09:31 +0200

    subtraktion metoden

--- GroupCalculator/Program.cs
+++ GroupCalculator/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+        static void Subtraction()
+        {
+            Console.WriteLine("Ange två heltal: ");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            int difference = userNum1 - userNum2;
+
+            Console.WriteLine($"Skillnaden blir: {difference}");
         }
     }
 }
